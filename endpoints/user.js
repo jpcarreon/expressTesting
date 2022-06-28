770dfb3c8f6c420dc6beae718482d601f40a7fee
cyjcardano <cecardano@up.edu.ph>
2022-06-28 16:31:44 +0800

    feat: update findUser and add updateUser

--- endpoints/user.js
+++ endpoints/user.js
@@ -7,5 +7,6 @@ const User = Router();
 // access controllers
 User.get('/', UserController.findUser); // localhost:3001/user/
 User.post('/', UserController.createUser);
+User.put('/', UserController.updateUser);
 
 module.exports = User;
