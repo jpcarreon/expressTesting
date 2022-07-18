6d97889d729f59af6a7677d3d89d328df8aa0218
jpcarreon <jccarreon3@up.edu.ph>
2022-07-18 16:23:54 +0800

    test: modify test

--- test/user.test.js
+++ test/user.test.js
@@ -2,9 +2,12 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const { start } = require('../app');
+require('dotenv').config();
 
 chai.use(chaiHttp);
 start();
+
+console.log(`${process.env.SERVER_PORT | 4001}`)
 
 describe('Regular Employee', () => {
     describe('[GET] /eps/employee/{empid}', () => {
@@ -12,7 +15,7 @@ describe('Regular Employee', () => {
             chai.request('localhost:3001')
                 .get('/user')
                 .end((err, response) => {
-                    assert.equal(response.statusCode, 200);
+                    assert.equal(response.statusCode, 404);
                     assert.isFalse(response.body.success);
 
                     done();
